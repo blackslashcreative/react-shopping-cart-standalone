a28d43bee2f1f925bc2f7dd34208edf660d531ac
Ghosty <ghosty@Ghostys-MacBook-Air.local>
2023-05-26 09:04:34 -0700

    Fixes restock urls

--- cart.jsx
+++ cart.jsx
@@ -189,9 +189,9 @@ const Products = (props) => {
       <Row>
         <form
           onSubmit={(event) => {
-            restockProducts(`http://localhost:1337/${query}`);
-            console.log(`Restock called on ${query}`);
             event.preventDefault();
+            restockProducts({query});
+            console.log(`Restock called on ${query}`);
           }}
         >
           <input
